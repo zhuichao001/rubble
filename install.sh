a5d249f551ae716aec4b8d309f626202a6a34341
zhuichao001 <zhuichao001@163.com>
2022-05-06 10:46:25 +0800

    build cmake and install

--- install.sh
+++ install.sh
@@ -1,1 +1,14 @@
+# 远程文件传送工具
+```
 yum install lrzsz
+```
+
+# cmake
+```
+yum install openssl openssl-devel
+wget https://github.com/Kitware/CMake/releases/download/v3.23.1/cmake-3.23.1.tar.gz
+tar xvzf cmake-3.23.1.tar.gz
+cd cmake-3.23.1
+./bootstrap
+make && make install
+```
