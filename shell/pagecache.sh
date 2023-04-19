bcd1557c0342cb3ef390d2a0c823aa72c45f29bc
zhuichao001 <zhuichao001@163.com>
2023-04-19 15:17:15 +0800

    modify

---
+++ shell/pagecache.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <PID>"
+    exit 1
+fi
+
+PID=$1
+
+if [ ! -d "/proc/${PID}" ]; then
+    echo "Process with PID ${PID} doest not exit."
+    exit
+fi
+
+PAGE_CACHE_KB=$(grep -e "^Cached:" /proc/${PID}/smaps | awk '{sum+=$2} END {print sum}')
+
+echo "Page Cache used by process ${PID}: ${PAGE_CACHE_KB} KB"
